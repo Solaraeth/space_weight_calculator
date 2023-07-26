af0174b0333bccd42524e368db58d8cf9c4def8b
Khthonian <anansicorp@tuta.io>
2023-07-26 23:37:18 +0100

    Add user input validation

--- space.cpp
+++ space.cpp
@@ -1,43 +1,61 @@
 #include <iostream>
 
 int main() {
-
     // Store the user's body weight on Earth
-
     float earthbw;
 
     // Store a switch variable for the user's planet choice
-
     int planet;
 
-    // Ask for user's Earth body weight
+    bool validInput = false;
 
-    std::cout << "Input Earth body weight (kg): ";
-    std::cin >> earthbw;
+    // Validate the user input
+    while (!validInput) {
+        // Ask for user's Earth body weight
+        std::cout << "Input Earth body weight (kg): ";
+        std::string weightInput;
+        std::cin >> weightInput;
 
+        // Try to convert the input string to a float
+        try {
+            earthbw = std::stof(weightInput);
+            validInput = true;
+        }     
 
-    // Ask user for desired planet to convert body weight to
-    
-    std::cout << "Planets:\n" 
-                "1 = Mercury\n"
-                "2 = Venus\n"
-                "3 = Mars\n"
-                "4 = Jupiter\n"
-                "5 = Saturn\n"
-                "6 = Uranus\n"
-                "7 = Neptune\n"
-                "Input number for desired planet: ";
-    std::cin >> planet;
-
-    // Ask user to try again if input is invalid
-
-    while (planet < 1 || planet > 7) {
-
-        std::cout << "Error: Invalid Input.\nChoose between 1-7.\nTry again: ";
-        std::cin >> planet;
-
+        // Catch exceptions and give the user a retry prompt
+        catch (const std::exception& e) {
+            std::cout << "Invalid input. Please enter a valid floating-point number.\n";
+        }
     }
 
+    validInput = false;
+
+    // Validate the user input
+    while (!validInput) {
+        // Ask user for desired planet to convert body weight to    
+        std::cout << "Planets:\n" 
+                    "1 = Mercury\n"
+                    "2 = Venus\n"
+                    "3 = Mars\n"
+                    "4 = Jupiter\n"
+                    "5 = Saturn\n"
+                    "6 = Uranus\n"
+                    "7 = Neptune\n"
+                    "Input number for desired planet: ";
+        std::string planetInput;
+        std::cin >> planetInput;
+
+        // Try to convert the input string to a float
+        try {
+            planet = std::stoi(planetInput);
+            validInput = true;
+        }     
+
+        // Catch exceptions and give the user a retry prompt
+        catch (const std::exception& e) {
+            std::cout << "Invalid input. Please enter a valid integer.\n";
+        }
+    }
 
     // Convert Earth body weight variable to equivalent mass on user's chosen planet
     switch(planet) {
@@ -60,8 +78,6 @@ int main() {
             std::cout << "Your weight on Uranus would be: " << earthbw * 0.92 << "kg\n";
             break;
         default:
-            std::cout << "Your weight on Neptune would be: " << earthbw * 1.19 << "kg\n";
-          
-              
-            }
+            std::cout << "Your weight on Neptune would be: " << earthbw * 1.19 << "kg\n";                       
     }
+}
